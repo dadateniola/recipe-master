9271459c8de563d8f8b3322069dbdd026dcaa5f3
weird-samuel <fiyinfoluwasamuel580@gmail.com>
2024-05-25 12:23:12 +0100

    added dynamic logo

---
+++ components/Logo.tsx
@@ -0,0 +1,18 @@
+"use cient";
+
+import useIsMobile from "@/hooks/UseIsMobile";
+import Image from "next/image";
+
+export default function Logo() {
+  const isMobile = useIsMobile();
+  return (
+    <div>
+      {/* Render different images based on the value of isMobile */}
+      {isMobile ? (
+        <Image src={"/logo/icon.png"} alt="Logo" width={141} height={26} />
+      ) : (
+        <Image src={"/logo/logo.png"} alt="Logo" width={141} height={26} />
+      )}
+    </div>
+  );
+}
