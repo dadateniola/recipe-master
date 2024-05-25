e1b5ed107bd6afec93fbb48e7e1fe06ae79fdc3f
weird-samuel <fiyinfoluwasamuel580@gmail.com>
2024-05-25 12:43:11 +0100

    file structure for auth

---
+++ app/auth/create-an-account/page.tsx
@@ -0,0 +1,9 @@
+export default function CreateAnAccount() {
+  return (
+    <div>
+      <h1 className="text-green-500">Create an Account</h1>
+      <p>Sign up for a new account</p>
+      {/* <CreateAnAccountForm /> */}
+    </div>
+  );
+}
---
+++ app/auth/login-to-your-account/page.tsx
@@ -0,0 +1,9 @@
+export default function LoginToYourAccount() {
+  return (
+    <div>
+      <h1 className="text-green-500">Login to Your Account</h1>
+      <p>Sign in to your account</p>
+      {/* <LoginToYourAccountForm /> */}
+    </div>
+  );
+}
