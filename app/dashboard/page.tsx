6c709e1b47f9798458924f098998b81a2415c49b
Dada Teniola <42898812+emmy13@users.noreply.github.com>
2024-05-28 14:42:23 +0100

    dashboard intro added

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,18 +1,54 @@
 //DASHBOARD
-import { Metadata } from "next";
+import { Metadata } from "next"
+import Image from "next/image";
 
 //Changing meta information of this page to something that better fits its use
 export const metadata: Metadata = {
   title: "Dashboard",
-  description: "Overview your recipe master account and view other chefs like you.",
+  description:
+    "Overview your recipe master account and view other chefs like you.",
 };
 
 const Dashboard = () => {
   return (
     <main className="dashboard">
-      Dashboard
+      <div className="dash-intro">
+        <div className="dash-intro-img">
+          <Image
+            src="/showcase/dash.jpg"
+            alt="intro image"
+            fill
+            sizes="70vw"
+          />
+        </div>
+        <div className="item stats">
+          <div className="heading">
+            <p>Your stats</p>
+          </div>
+          <div className="data res">
+            <p>18</p>
+            <span>recipes</span>
+          </div>
+          <div className="row">
+            <div className="data cat">
+              <p>2</p>
+              <span>categories</span>
+            </div>
+            <div className="data other">
+              <span>Other users</span>
+              <Image
+                src="/icons/arrow.png"
+                alt="arrow"
+                width={60}
+                height={60}
+                sizes="50px"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
     </main>
-  )
-}
+  );
+};
 
-export default Dashboard
+export default Dashboard;
